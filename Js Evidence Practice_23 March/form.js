34edbd432ef3b1eb1f9cc35a85dc0a608f034921
Shamima-Nasrin10 <72626163+Shamima-Nasrin10@users.noreply.github.com>
2024-03-23 16:32:44 +0600

    23 March
    Js Evidence practice

---
+++ Js Evidence Practice_23 March/form.js
@@ -0,0 +1,34 @@
+
+
+
+function submitForm(event) {
+
+    event.preventDefault();
+
+    let fName = document.getElementById('name').value;
+
+    let sex = document.querySelector('input[name="sex"]:checked').value;
+
+    let courses = document.querySelectorAll('input[name="courses"]:checked');
+
+    let location = document.getElementById('location').value;
+
+
+    let coursesArray = [];
+    for (let i = 0; i < courses.length; i++) {
+        coursesArray.push(courses[i].value);
+    }
+
+    let output = 'Full Name: ' + fName + '\n';
+    output += 'Sex: ' + sex + '\n';
+    output += 'Courses: ' + coursesArray + '\n';
+    output += 'Location: ' + location + '\n';
+
+    let newWindow = window.open("", '_blank');
+    newWindow.document.write("<pre>" + output + "</pre>");
+
+
+}
+
+let myForm = document.getElementById('myForm');
+myForm.addEventListener('submit', submitForm);
